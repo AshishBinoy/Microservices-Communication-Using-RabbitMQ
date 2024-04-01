9b95131f61e6f91fcddb51c3f01ac806ba9fc8cb
Ashish Binoy <abraham70001@gmail.com>
2024-04-01 20:12:09 +0530

    item creation does not work

---
+++ consumer_three/stock_management.py
@@ -0,0 +1,34 @@
+import mysql.connector 
+import sys
+import pika 
+
+
+
+#Creating MySQL Connection 
+mydb = mysql.connector.connect(host = "localhost", user = "root", password = "123456789")
+cursor = mydb.cursor() 
+cursor.execute("USE Inventory")
+
+#Creating RabbitMQ Connection
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='exchange', exchange_type='direct')
+channel.queue_declare(queue='stock')
+
+channel.queue_bind(exchange='exchange', queue='stock', routing_key='stock')
+
+# Function to update stock 
+
+def callback(ch, method, properties, body):     
+    print(f"Received message: {body}")
+    item_id, item_name, item_price, item_quantity = body.decode('utf-8').split(":")
+    cursor.execute("UPDATE Items SET item_quantity = %s WHERE item_id = %s", (item_quantity, item_id))
+    mydb.commit()
+    print(f"Stock of {item_name} updated to {item_quantity}")
+
+
+
+print("Waiting for message... press Ctrl + C to exit.")
+channel.basic_consume(queue='stock', on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
+
