ef635dd360c270b4e32d42ee0c07a6860f69856d
Ashish Binoy <abraham70001@gmail.com>
2024-04-01 20:12:35 +0530

    item creation does not work

--- consumer_two/item_creation.py
+++ consumer_two/item_creation.py
@@ -1,7 +1,7 @@
 import mysql.connector 
 import pika 
 
-
+# DOES NOT WORK NEED TO FIX
 
 #Creating MySQL Connection 
 mydb = mysql.connector.connect(host = "localhost", user = "root", password = "123456789")
