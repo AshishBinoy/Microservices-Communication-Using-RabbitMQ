35661ebfdc12fd9ae07acf740bbb12abe71e2d46
Jimzical <arishkumar2003@gmail.com>
2024-04-15 10:56:39 +0530

    Update Dockerfiles and docker-compose.yml for all consumers and the producer

--- producer/producer.py
+++ producer/producer.py
@@ -4,19 +4,18 @@ import pika
 app = flask.Flask(__name__)
 
 
-# TODO: Add this after RabbitMQ is setup
-# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-# channel = connection.channel() 
-# channel.exchange_declare(exchange='exchange', exchange_type='direct')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel() 
+channel.exchange_declare(exchange='exchange', exchange_type='direct')
 
-# channel.queue_declare(queue='healthcheck')
-# channel.queue_declare(queue='read')
-# channel.queue_declare(queue='order_processing')
-# channel.queue_declare(queue='item_creation')
+channel.queue_declare(queue='healthcheck')
+channel.queue_declare(queue='read')
+channel.queue_declare(queue='order_processing')
+channel.queue_declare(queue='item_creation')
 
-# binding_keys = ['healthcheck', 'read', 'order_processing', 'item_creation']
-# for binding_key in binding_keys:
-#     channel.queue_bind(exchange='exchange', queue=binding_key, routing_key=binding_key)
+binding_keys = ['healthcheck', 'read', 'order_processing', 'item_creation']
+for binding_key in binding_keys:
+    channel.queue_bind(exchange='exchange', queue=binding_key, routing_key=binding_key)
 
 
 
@@ -28,8 +27,7 @@ def home():
 def health_check():
     message = "Health check message sent"
 
-    # TODO: Add this after RabbitMQ is setup
-    # channel.basic_publish(exchange='exchange', routing_key='healthcheck', body=message)
+    channel.basic_publish(exchange='exchange', routing_key='healthcheck', body=message)
 
     return message
 
@@ -37,8 +35,7 @@ def health_check():
 def read():
     message = "Message to retrieve all records sent"
 
-    # TODO: Add this after RabbitMQ is setup
-    # channel.basic_publish(exchange='exchange', routing_key='read', body=message)
+    channel.basic_publish(exchange='exchange', routing_key='read', body=message)
 
     return message
 
@@ -46,8 +43,7 @@ def read():
 def insert(item_name, item_price, item_quantity):
     message = f"{item_name}:{item_price}:{item_quantity}"
 
-    # TODO: Add this after RabbitMQ is setup
-    # channel.basic_publish(exchange='exchange', routing_key='item_creation', body=message)
+    channel.basic_publish(exchange='exchange', routing_key='item_creation', body=message)
 
     return message
 
@@ -56,8 +52,7 @@ def insert(item_name, item_price, item_quantity):
 def delete(item_id):
     message = f"{item_id}"
 
-    # TODO: Add this after RabbitMQ is setup
-    # channel.basic_publish(exchange='exchange', routing_key='order_processing', body=message)
+    channel.basic_publish(exchange='exchange', routing_key='order_processing', body=message)
 
     return message  
 
